76b4477d8acaa62cada4ca8a7b29e713b57c49e5
Zhang Jiale <2129449173@qq.com>
2025-09-24 08:55:44 +0800

    对上次的补交

--- C++/code/qsort.cpp
+++ C++/code/qsort.cpp
@@ -7,10 +7,10 @@ void quicksort(int(&)[10],int,int);
 int partition(int(&)[10],int,int);  // 分区函数
 void swap(int(&)[10],int,int);
 int main(){
-    int arr[10]={23,45,18,6,11,19,6,18,12,3};
+    int arr[10]={23,45,18,6,11,19,99,18,12,3};
     printArray(arr);
     quicksort(arr,0,9);
-    printArray(arr);
+    printArray(arr);  // 3 6 11 12 18 18 19 23 45 99 
     cin.get();
 }
 // 定义快速排序的函数,用递归实现
@@ -19,6 +19,10 @@ void quicksort(int(&a)[10],int start,int end){
 	if(start>=end)
 		return;
 	int mid = partition(a,start,end);
+	if (mid==-1){
+		cout << "分区函数出错了"  << endl;
+		return;
+	}
 	// 递归调用,分别对两部分继续排序
 	quicksort(a,start,mid-1);
 	quicksort(a,mid+1,end);
@@ -29,13 +33,13 @@ int partition(int(&a)[10],int start,int end){
 
 	// 指定指向数组头尾元素的"指针"
 	int l=start,r=end;
-	while(l<=r){
+	while(l<r){
 		// l指针不断向右移动,直到找到一个比支点大的值
 		// r指针不断向左移动,直到找到一个比支点小的值
-		while(a[l]<=pivot && l<=r){
+		while(a[l]<=pivot && l<r){
 			l++;
 		}
-		while(a[r]>=pivot && l<=r){
+		while(a[r]>=pivot && l<r){
 			r--;
 		}
 		// 左右互换
@@ -52,8 +56,10 @@ int partition(int(&a)[10],int start,int end){
 		swap(a,start,l-1);
 		return l-1;
 	}
+	return -1;
 
 }
+
 void printArray(const int(&a)[10]){
 	for(int num:a){
 		cout << num << " ";
